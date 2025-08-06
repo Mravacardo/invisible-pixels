965f94f0817926634585f6084ef6a2da753c01e8
Mravacardo <matthewheathcote@icloud.com>
2025-08-06 17:00:09 +0100

    Main.py

--- Main.py
+++ Main.py
@@ -1,15 +1,15 @@
 # replace the red pixels ( or undesired area ) with 
 # background pixels to generate the invisibility feature. 
   
-## 1. Hue: This channel encodes color information. Hue can be 
-# thought of an angle where 0 degree corresponds to the red color,  
-# 120 degrees corresponds to the green color, and 240 degrees  
-# corresponds to the blue color. 
+## 1. Hue: This channel encodes colour information. Hue can be 
+# thought of an angle where 0 degree corresponds to the red colour,  
+# 120 degrees corresponds to the green colour, and 240 degrees  
+# corresponds to the blue colour. 
   
-## 2. Saturation: This channel encodes the intensity/purity of color. 
+## 2. Saturation: This channel encodes the intensity/purity of colour. 
 # For example, pink is less saturated than red. 
   
-## 3. Value: This channel encodes the brightness of color. 
+## 3. Value: This channel encodes the brightness of colour. 
 # Shading and gloss components of an image appear in this
 
 import cv2
@@ -30,3 +30,25 @@ for i in range(60):
         continue
 
 background = np.flip(background, axis = 1)
+
+while (capture_video.isOpened())
+    return_val, img = capture_video.read()
+    if not return_val:
+        break
+    count = count + 1
+    img = np.flip(img, axis = 1)
+
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    lower_red = np.array([100, 40, 40])
+    upper_red = np.array([100, 255, 255])
+    mask1 = cv2.inrRange(hsv, lower_red, upper_red)
+
+    lower_red = np.array([155, 40, 40])
+    upper_red = np.array([180, 255, 255])
+    mask2 = cv2.inRange(hsv, lower_red, upper_red)
+
+    mask1 = mask1 + mask2
+    mask1 = cv2.morphologyEx(mask1, cv2.MORPH_OPEN, np.ones((3, 3), 
+                                         np.uint8), iterations = 2)
+    mask1 = cv2.dilate(mask1, np.ones((3, 3), ))
